20c0287bcaf0a200c11888ac2b0d1b9d51844c11
Butter909 <152233512+Butter909@users.noreply.github.com>
2024-08-11 06:51:35 -0400

    Update AppDelegate.swift

--- MortgageCalculator/MortgageCalculator/AppDelegate.swift
+++ MortgageCalculator/MortgageCalculator/AppDelegate.swift
@@ -10,29 +10,20 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
-    
+    // application has finished launching
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        // Customize after the application has launched
         return true
     }
-    
 
-    // MARK: UISceneSession Lifecycle
-
+    // A new scene session is being created
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
+        // Selects a configuration to create the new scene 
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    // user discards a scene session
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+       
     }
-
-
 }
-
